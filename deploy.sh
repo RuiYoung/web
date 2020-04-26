af442ebd7e6f06a5cad3c2e9ec4e13c5191f489b
Ruiyoung <714021656@qq.com>
2020-04-26 14:13:55 +0800

    gh-pages

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:RuiYoung/web.git master:master
+git push -f git@github.com:RuiYoung/web.git master:gh-pages
 
 cd -
